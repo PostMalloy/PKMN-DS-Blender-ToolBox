82f08470a52f39efc80885b037ad2810171d9ba8
PostMalloy <maxgriswold77@gmail.com>
2025-04-18 11:23:30 -0400

    Initial skeleton for operator

---
+++ operators/Set_Camera.py
@@ -0,0 +1,25 @@
+import bpy
+import math
+
+class SetCameraHGSSInterior(bpy.types.Operator):
+    """Split object by VC and export all resulting meshes at once"""          # Use this as a tooltip for menu items and buttons.
+    bl_idname = "object.set_camera_hgss_interior"      # Unique identifier for buttons and menu items to reference.
+    bl_label = "Set camera perspective, zoom and angle to match HGSS interior camera"      # Display name in the interface.
+    bl_options = {'REGISTER', 'UNDO'}      # Enable undo for the operator.
+
+    def execute(self,context):
+
+        def set_viewport_angle_and_zoom(angle_z=45):
+            for area in bpy.context.screen.areas:
+                if area.type == 'VIEW_3D':
+                    for space in area.spaces:
+                        if space.type == 'VIEW_3D':
+                            region_3d = space.region_3d
+                            region_3d.view_rotation = (math.radians(angle_z), 0.15, 0.0, 0.0) #dial in exact settings later
+                            region_3d.view_distance = 11 #dial in later
+                            region_3d.view_perspective = 'ORTHO'
+                            return
+
+        set_viewport_angle_and_zoom()
+
+    return {'FINISHED'}
