488d9b3e1291f760f17a6d8ede0a4e18bfd15597
PostMalloy <maxgriswold77@gmail.com>
2025-04-18 11:23:17 -0400

    Initial skeleton for operator

---
+++ operators/Split_and_Export.py
@@ -0,0 +1,18 @@
+import bpy
+import os
+
+from bpy import context
+from bpy.props import StringProperty, BoolProperty
+from bpy.types import Operator
+
+from .operators import Split_By_VC2, Export_PDSMS_obj
+
+class PDSMSSplitExport(bpy.types.Operator):
+    """Split object by VC and export all resulting meshes at once"""          # Use this as a tooltip for menu items and buttons.
+    bl_idname = "object.export_and_split"      # Unique identifier for buttons and menu items to reference.
+    bl_label = "Split and Export all as .obj"      # Display name in the interface.
+    bl_options = {'REGISTER', 'UNDO'}      # Enable undo for the operator.
+
+    def execute(self,context):
+        #tbd
+    return {'FINISHED'}
